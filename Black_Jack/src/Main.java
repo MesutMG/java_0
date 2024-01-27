cc3684a16f796ba7fd317445c060c60b5bb47a5b
Mesut <mesutahmettravaci@gmail.com>
2024-01-28 00:10:33 +0300

    Update Main.java

--- Black_Jack/src/Main.java
+++ Black_Jack/src/Main.java
@@ -1,20 +1,22 @@
 import java.util.Arrays;
 import java.util.Random;
+import java.util.ArrayList;
+import java.util.List;
 
 public class Main {
     public static void main(String[] args) {
 
-        String[] card_signs = {"S","D","H","C"};
+        String[] card_signs = {" of Spades"," of Diamonds"," of Hearts"," of Clubs"};
         String[] card_numbers = {"2","3","4","5","6","7","8","9","10","J","Q","K","A"};
+        List<String> combinations = new ArrayList<>();
         int[] card_values = {2,3,4,5,6,7,8,9,10,10,10,10,11};
 
 
+                for (String c1 : card_numbers) {
+                    for (String c2 : card_signs) {
+                        combinations.add(c1 + c2);
+                    }
 
-    }
-
-
-    public static int card_select(int n){
-        for(int i=0;)
-        return 0;
-    }
+                }
+            }
 }
