4b5f1e4c74cc6d6ef24493efaa6281f270ccea79
Mesut <mesutahmettravaci@gmail.com>
2024-01-27 23:56:27 +0300

    black_jack
    almost not even started

---
+++ Black_Jack/src/Main.java
@@ -0,0 +1,20 @@
+import java.util.Arrays;
+import java.util.Random;
+
+public class Main {
+    public static void main(String[] args) {
+
+        String[] card_signs = {"S","D","H","C"};
+        String[] card_numbers = {"2","3","4","5","6","7","8","9","10","J","Q","K","A"};
+        int[] card_values = {2,3,4,5,6,7,8,9,10,10,10,10,11};
+
+
+
+    }
+
+
+    public static int card_select(int n){
+        for(int i=0;)
+        return 0;
+    }
+}
