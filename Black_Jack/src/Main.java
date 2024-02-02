d2ea81bc3de4cead95e97f68a628faffd2c2efe4
mesutmg <mesutahmettravaci@gmail.com>
2024-02-02 23:54:16 +0300

    to-pc and tiny blackjack ups

--- Black_Jack/src/Main.java
+++ Black_Jack/src/Main.java
@@ -3,20 +3,24 @@ import java.util.Random;
 import java.util.ArrayList;
 import java.util.List;
 
+
 public class Main {
     public static void main(String[] args) {
 
-        String[] card_signs = {" of Spades"," of Diamonds"," of Hearts"," of Clubs"};
-        String[] card_numbers = {"2","3","4","5","6","7","8","9","10","J","Q","K","A"};
+        String[] card_signs = {" of Spades", " of Diamonds", " of Hearts", " of Clubs"};
+        String[] card_numbers = {"2", "3", "4", "5", "6", "7", "8", "9", "10", "Jack", "Queen", "King", "Ace"};
         List<String> combinations = new ArrayList<>();
-        int[] card_values = {2,3,4,5,6,7,8,9,10,10,10,10,11};
 
-
-                for (String c1 : card_numbers) {
-                    for (String c2 : card_signs) {
-                        combinations.add(c1 + c2);
-                    }
-
-                }
+        for (String c1 : card_numbers) {
+            for (String c2 : card_signs) {
+                combinations.add(c1 + c2);
             }
+        }
+        /*
+        if (str.startsWith(arg)){
+            return arg, args value
+        } else if {
+
+        }*/
+    }
 }
